4d9a3974749e057f9e7d45f034fbc8a3e5cf5cd4
Paul <paul.kiplagt@student.moringaschool.com>
2022-09-26 20:48:37 +0300

    anothe one

--- db/migrate/20220926174719_create_students.rb
+++ db/migrate/20220926174719_create_students.rb
@@ -2,7 +2,8 @@ class CreateStudents < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
       t.string :first_name
-      t.grade :last_name
+      t.string :last_name
+      t.integer :grade
 
       t.timestamps
     end
